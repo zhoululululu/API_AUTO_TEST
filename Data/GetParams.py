1dc7383c4eb8394049b880bf5fb8eb8a6cf6fde2
zhoululululu <chloe@wemart.cn>
2019-06-11 18:31:02 +0800

    0611update

--- Data/GetParams.py
+++ Data/GetParams.py
@@ -13,42 +13,34 @@ class GetParams:
 
     def translation_params(self, model, api_func):
         api_param = []
-        data_param = []
+        data_param = [] #API参数
         param = {}
         trans_params = []
 
         required_params = GetAPI.get_required_params(self, model, api_func)
         selective_params = GetAPI.get_selective_params(self, model, api_func)
         params_data = Config.get_case_params(self, api_func)
-        print(params_data)
 
         if required_params == "-" and selective_params == "-":
             return trans_params
 
         elif required_params != "-" and selective_params == "-":
+            # 先将API参数组合为list
             pa = str(required_params).split(",")
-            # d_pa = str(params_data).split(",")
-            print(pa)
-            # print(d_pa)
             for i in range(len(pa)):
-                param[pa[i]] = ""
-                for j in range(len(params_data)):
-                    print((params_data[j].split(",")))
-                    param[pa[i]] = params_data[j].split(",")[i]
-                    data_param.append(param)
-            print(data_param)
-            return trans_params
+                param[pa[i]] =""
+            data_param.append(param)
+            # print(data_param)
 
-        print(required_params, selective_params)
-        # all_case_data = Config.get_case_params(self, fileName)
-        # for i in range (len(all_case_data)):
-        #     params = all_case_data[i][1]
-        #     print(params)
-        # print(all_case_data)
+            for j in range(len(params_data)):
+                print(data_param[j])
 
+                # param = params_data[j].split(",")
+                # print(param)
+                # data_param.insert(j,param)
 
+        return data_param
 test = GetParams()
-# test.get_params("Login", "Login")
 test.translation_params("Login", "Login")
 # test.translation_params("Logout","Logout")
 # test.translation_params("-","-")
